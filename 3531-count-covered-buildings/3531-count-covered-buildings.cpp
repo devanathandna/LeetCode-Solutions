c35a41d348f9aef09d967d08670fc8814be0ca25
Deavanathan_S <148757701+devanathandna@users.noreply.github.com>
2025-12-11 11:28:32 +0530

    Time: 1451 ms (11.05%), Space: 483.6 MB (8.29%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        unordered_map<int, set<int>> rows, cols;
-        for (auto &b : buildings) {
-            int x = b[0], y = b[1];
-            rows[x].insert(y);
-            cols[y].insert(x);
+        unordered_map<int,set<int>>rows,cols;
+        for(auto it:buildings){
+            int x = it[0] , y = it[1];
+            rows[x].insert(it[1]);
+            cols[y].insert(it[0]);
         }
+        int count=0;
+        for(auto it:buildings){
+            int x = it[0] ,y = it[1];
+            bool up = false , down = up,left = down,right = left;
+            auto idx = rows[x].lower_bound(y);
+            if(idx!=rows[x].begin()) down = true;
+            idx = rows[x].upper_bound(y);
+            if(idx!=rows[x].end()) up = true;
 
-        int count = 0;
-        for (auto &b : buildings) {
-            int x = b[0], y = b[1];
-            bool left = false, right = false;
-            auto it = rows[x].lower_bound(y);
-            if (it != rows[x].begin()) left = true;
-            ++it;
-            if (it != rows[x].end()) right = true;
+            auto idy = cols[y].lower_bound(x);
+            if(idy!=cols[y].begin()) left = true;
+            idy = cols[y].upper_bound(x);
+            if(idy!=cols[y].end()) right = true;
 
-    
-            bool down = false, up = false;
-            auto it2 = cols[y].lower_bound(x);
-            if (it2 != cols[y].begin()) down = true;
-            ++it2;
-            if (it2 != cols[y].end()) up = true;
-
-            if (left && right && up && down) {
+            if(left && right && up && down){
                 count++;
             }
         }
-
         return count;
     }
 };
