60e0264915a7d520295c9c4620789f12b466ec6b
Deavanathan S <148757701+devanathandna@users.noreply.github.com>
2026-01-22 09:34:23 +0530

    Time: 0 ms (100%), Space: 18.3 MB (31.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,33 +1,36 @@
-const int DX[] = {-1,0,1,0};
-const int DY[] = {0,1,0,-1};
-
 class Solution {
 public:
-    
+    const int dx[4] = {-1, 0, 1, 0};
+    const int dy[4] = {0, 1, 0, -1};
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
-        //visited[sr][sc] = 1;
-        int oldcolor = image[sr][sc];
-        image[sr][sc] = color;
-        q.push({sr,sc});
-        while(!q.empty()){
-            int ii = q.front().first;
-            int jj = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int x = ii + DX[i];
-                int y = jj + DY[i];
 
-                if(x>=0 && x < n && y>=0 && y<m && visited[x][y]==0 && image[x][y]==oldcolor){
-                    image[x][y] = color;
-                    visited[x][y] = 1;
-                    q.push({x,y});
+        int oldColor = image[sr][sc];
+        if (oldColor == color) return image;  // nothing to do
+
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;   // mark visited by repainting
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
+                    image[nx][ny] == oldColor) {
+
+                    image[nx][ny] = color;
+                    q.push({nx, ny});
                 }
             }
         }
+
         return image;
     }
 };
